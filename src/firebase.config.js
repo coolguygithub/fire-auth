44a613a38818108fdd31df3025cd7c43f85612e1
Rakib Hassan Milon <rakib.coolguy@gmail.com>
2021-03-14 08:04:24 +0600

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAGGwL-ASNYerylPCUdOAst5OGc5xICFrk",
+    authDomain: "atom-bd-8f2e4.firebaseapp.com",
+    projectId: "atom-bd-8f2e4",
+    storageBucket: "atom-bd-8f2e4.appspot.com",
+    messagingSenderId: "617984688279",
+    appId: "1:617984688279:web:ff9c0cc805fbf57e3fe9d2"
+  };
+
+export default firebaseConfig;
